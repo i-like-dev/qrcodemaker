a29d5235c436ca5b83076376f38e0748584b6769
i-like-dev <136171623+i-like-dev@users.noreply.github.com>
2023-08-18 22:18:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const generateQRButton = document.getElementById("generateQR");
-    const inputText = document.getElementById("inputText");
-    const imageInput = document.getElementById("imageInput");
-    const downloadLink = document.getElementById("downloadLink");
-    const qrcodeContainer = document.getElementById("qrcode");
+$(document).ready(function () {
+    const generateQRButton = $("#generateQR");
+    const qrTypeSelect = $("#qrType");
+    const inputText = $("#inputText");
+    const imageInput = $("#imageInput");
+    const logoInput = $("#logoInput");
+    const downloadLink = $("#downloadLink");
+    const qrcodeContainer = $("#qrcode");
 
-    generateQRButton.addEventListener("click", function () {
-        const text = inputText.value;
+    generateQRButton.click(function () {
+        const qrType = qrTypeSelect.val();
+        const text = inputText.val();
 
-        if (text) {
-            qrcodeContainer.innerHTML = ""; // 清除之前的QR碼
-
-            const qr = new QRious({
-                element: qrcodeContainer,
-                value: text,
-                size: 200,
-            });
-
-            downloadLink.href = qr.toDataURL();
-            downloadLink.style.display = "block";
+        if (qrType === "text" && text) {
+            generateQRCode(text);
+        } else if (qrType === "sms" && text) {
+            generateQRCode("SMSTO:" + text);
+        } else if (qrType === "email" && text) {
+            generateQRCode("mailto:" + text);
+        } else if (qrType === "wifi" && text) {
+            generateQRCode("WIFI:" + text);
+        } else if (qrType === "link" && text) {
+            generateQRCode(text);
         }
     });
 
-    imageInput.addEventListener("change", function () {
-        const file = imageInput.files[0];
+    logoInput.change(function () {
+        const file = logoInput[0].files[0];
 
         if (file) {
             const reader = new FileReader();
 
             reader.onload = function (event) {
-                qrcodeContainer.innerHTML = ""; // 清除之前的QR碼
-
-                const qr = new QRious({
-                    element: qrcodeContainer,
-                    value: event.target.result,
-                    size: 200,
+                const logoData = event.target.result;
+                qrcodeContainer.empty();
+                const qr = new QRCodeWithLogo({
+                    content: inputText.val(),
+                    logo: logoData,
+                    container: qrcodeContainer,
+                    width: 200,
+                    height: 200,
                 });
 
-                downloadLink.href = qr.toDataURL();
-                downloadLink.style.display = "block";
+                downloadLink.attr("href", qr.toDataURL());
+                downloadLink.css("display", "block");
             };
 
             reader.readAsDataURL(file);
         }
     });
+
+    function generateQRCode(content) {
+        qrcodeContainer.empty();
+        const qr = new QRious({
+            element: qrcodeContainer[0],
+            value: content,
+            size: 200,
+        });
+
+        downloadLink.attr("href", qr.toDataURL());
+        downloadLink.css("display", "block");
+    }
 });
